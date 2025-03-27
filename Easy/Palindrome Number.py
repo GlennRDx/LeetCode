2e3ae675c5bd18209a2b2cf50987dbddf841d37a
Glenn Ross-Dolan <glennrossdolan@gmail.com>
2025-03-27 14:18:32 +0000

    Update Palindrome Number.py

--- Easy/Palindrome Number.py
+++ Easy/Palindrome Number.py
@@ -1,4 +1,8 @@
 class Solution:
+    """
+    Converts the int to a string, checks if it's equal to the reverse of the string. 
+    Returns true/false
+    """
     def isPalindrome(self, x: int) -> bool:
         integer = []
         if str(x) == str(x)[::-1]:
